ed947cdcfc1cf251c285266b2658cf8bbd5b070b
Raveesh Agarwal <hi@raveeshagarwal.in>
2020-09-16 10:15:32 +0530

    adds quiver package for more readable implementations

--- lib/multiples_of_3_n_5.dart
+++ lib/multiples_of_3_n_5.dart
@@ -1,6 +1,8 @@
+import 'package:quiver/iterables.dart';
+
 sumOfMultiplesLessThan(int number) {
   final Set<int> multiples = Set();
-  for (number in List.generate(number - 3, (index) => index + 3))
+  for (number in range(3, number))
     if (number % 3 == 0 || number % 5 == 0) multiples.add(number);
   return multiples.reduce((value, element) => value + element);
 }
